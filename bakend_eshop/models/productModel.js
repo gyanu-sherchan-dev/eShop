9ceb00c963b1720f604c37622fbd5f1525c4c0e2
Gyanu <gyanusherchan@outlook.com>
2023-07-17 19:58:15 +1000

    added all data model (product, user, order)

---
+++ bakend_eshop/models/productModel.js
@@ -0,0 +1,84 @@
+//this is where we wanted created our Schema and exported as model.
+
+import mongoose from "mongoose";
+
+//reviewSchema
+const reviewSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      // we created this user field, because there need to have a connection between product and user who created that, in our case admin will be the user.
+      type: mongoose.Schema.Types.ObjectId, // this is the id which is created by mongo when user is created (_id)
+      required: true,
+      ref: "User", //refers to user collection, where the objectId is comming from
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Products", productSchema);
+
+export default Product;
---
+++ bakend_eshop/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false, //so inorder ot make an admin, we have to go into database and change that to true.
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
