528ac139329ba24471b8a3dec9cb095cb56584af
Gyanu <gyanusherchan@outlook.com>
2023-07-17 14:07:41 +1000

    express Server and intial product route and env variable setup

---
+++ bakend_eshop/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import products from "./data/Products.js";
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+//serving all the products
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+//route for single products
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
