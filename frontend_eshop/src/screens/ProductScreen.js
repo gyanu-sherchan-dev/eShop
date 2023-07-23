321d627015db565e188e1d43a04342a7c8d85a0b
Gyanu <gyanusherchan@outlook.com>
2023-07-23 15:09:37 +1000

    cartSlice created with addToCart reducer func and call it using dispatch from addToCartHandler func

--- frontend_eshop/src/screens/ProductScreen.js
+++ frontend_eshop/src/screens/ProductScreen.js
@@ -1,16 +1,27 @@
-// import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import Rating from "../components/Rating";
 // import axios from "axios";
 import { useGetProductDetailsQuery } from "../slices/productApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import { addToCart } from "../slices/cartSlice";
 
 const ProductScreen = () => {
   // const [product, setProduct] = useState({});
   const { id: productId } = useParams();
+  const [qty, setQty] = useState(1);
 
   // useEffect(() => {
   //   const fetchProduct = async () => {
@@ -26,12 +37,22 @@ const ProductScreen = () => {
   //   fetchProduct();
   // }, [productId]);
 
+  //we also need to bring or useDispatch because we can not call addToCart directly, we have to call dispatch it.
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   //redux:
   const {
     data: product,
     isLoading,
     error,
   } = useGetProductDetailsQuery(productId);
+
+  const addToCartHandler = () => {
+    //call the addToCart action that we created in cartSlice
+    dispatch(addToCart({ ...product, qty }));
+    navigate("/cart");
+  };
 
   return (
     <>
@@ -91,11 +112,34 @@ const ProductScreen = () => {
                       </Col>
                     </Row>
                   </ListGroup.Item>
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty:</Col>
+                        <Col>
+                          <Form.Control
+                            as="select"
+                            value={qty}
+                            onChange={(e) => setQty(Number(e.target.value))}
+                          >
+                            {[...Array(product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
                   <ListGroup.Item>
                     <Button
                       className="btn-block"
                       type="button"
                       disabled={product.countInStock === 0}
+                      onClick={addToCartHandler}
                     >
                       Add To Cart
                     </Button>
---
+++ frontend_eshop/src/slices/cartSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+//in other slice before we use create API because that was the slice where we have endPoints that are dealing with asynchronous requests, but we are not doing that with cart, so we do not need to use create API, we can simply use create slice function
+
+//our items are going to store in local storage, so that when we leave the site, we come back, our items are still in the cart, so we want to check that localstorage item first.
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [] };
+
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      //state is the current state of the cart and action will include any data inside of a payload, so in this case, we are going to be sending an item to add to the cart which we can access with action.payload
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      //calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+      //calculate shipping price (if order is over $100 then free, else $10 shipping )
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+      //calculate tax price
+      state.taxPrice = addDecimals(
+        Number((0.15 * state.itemsPrice).toFixed(2))
+      );
+      //calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
+  //in this reducer object we will have any funtions that have to do with the cart, so when we add to cart, remove etc..
+  //anything we create in reducers such as add to cart, remove from cart, save shipping address, need to export those as actions, but we also want to export the reducers altogether and bring into our store file
+});
+
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
