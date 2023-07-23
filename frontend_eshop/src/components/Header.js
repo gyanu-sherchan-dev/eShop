9ef63d65e50dc7c5226f68e881401b544bc4dfb1
Gyanu <gyanusherchan@outlook.com>
2023-07-23 16:30:47 +1000

    item count funtionality added in Header.js

--- frontend_eshop/src/components/Header.js
+++ frontend_eshop/src/components/Header.js
@@ -1,11 +1,14 @@
-import Nav from "react-bootstrap/Nav";
+import { Nav, Badge } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 // import logo from "../assets/logoo.png";
+import { UseSelector, useSelector } from "react-redux";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  console.log(cartItems);
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -23,6 +26,11 @@ const Header = () => {
                 <Nav.Link>
                   <FaShoppingCart />
                   Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((acc, i) => acc + i.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/cart">
